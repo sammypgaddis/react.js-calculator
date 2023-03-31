b2bfd12b09cb32f729682970aa9a98672fbd942b
Samuel Gaddis <siddag14@live.com>
2023-03-31 14:35:22 -0400

    ryan and I completed the css and layout of calculator grid. next step, create the functionality of the buttons

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import { useReducer } from "react";
+
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+}
+
+function reducer(state, { type, payload }) {
+  // switch(type) {
+  //   case ACTIONS.ADD_DIGIT:
+  //   return {
+  //     ...state, 
+  //     currentOperand: `${currentOperand}${payload.digit}`
+  //   }
+  // }
+}
 
 function App() {
+const [{ currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
+
+// dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit: 1}})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome back Ryan and Sam! Get this Calculator done!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className= "previous-operand">{previousOperand} {operation}</div>
+        <div className= "current-operand">{currentOperand}</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+
     </div>
   );
+  
 }
 
 export default App;
