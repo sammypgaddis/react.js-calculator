59d53ce8609b37ba3ecdcd7fe488feb132c11ad8
Samuel Gaddis <siddag14@live.com>
2023-03-31 13:02:17 -0400

    sam pushes his first commit for the side project

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome back Ryan and Sam! Get this Calculator done!
         </p>
         <a
           className="App-link"
