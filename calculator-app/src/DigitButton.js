73573577da9b72459636eb654fbd2fe775ad8c37
Samuel Gaddis <siddag14@live.com>
2023-04-05 17:27:03 -0400

    created the operationbutton.js component. just have to do some clean up with css and inside of the app.js component

--- calculator-app/src/DigitButton.js
+++ calculator-app/src/DigitButton.js
@@ -1,11 +1,11 @@
 import { ACTIONS } from "./App"
 
-export default function DigitButton({dispatch, digit}){
-    return (
+export default function DigitButton({ dispatch, digit }) {
+  return (
     <button
-    onClick={()=> dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit} })} 
-     >
-    {digit}
-        </button>
-    )
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
 }
